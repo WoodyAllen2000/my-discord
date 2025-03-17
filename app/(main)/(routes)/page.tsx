661c25de27e9edda5bcde5b4614ceecb6c0a534e
chengning <ningc673@gmail.com>
2025-03-17 18:34:49 +0800

    登录注册，middleware中间件验证，明暗模式切换

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
+      </div>
+
+    </div>
+  );
+}
